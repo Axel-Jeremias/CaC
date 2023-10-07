25f032258be8e4d980d4648b56c2f51d6e55eb06
Axel-Jeremias <axeljeremias1@gmail.com>
2023-10-07 17:54:06 -0300

    JS slider

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,22 +1,32 @@
-function slider() {
-    let currentSlider = 1
+let currentSlider = 1;
 
-    return function loopSlides() {
-        const slides = document.getElementsByClassName("slide");
-
-        if (currentSlider >= slides.length) {
-            currentSlider = 1
-        } else {
-            currentSlider++;
-        }
-
-        for (i = 0; i < slides.length; i++) {
-            slides[i].style.display = 'none';
-        }
-
-        slides[currentSlider - 1].style.display = 'block';
+function loopSlides(novedades) {
+    if (currentSlider >= novedades.length) {
+        currentSlider = 1;
+    } else {
+        currentSlider++;
     }
 
+    const novedadActual = novedades[currentSlider - 1];
+
+    const elementoTitulo = document.querySelector(".slide > .info-slide > h3");
+    const elementoParrafo = document.querySelector(".slide > .info-slide > p");
+    const elementoImagen = document.querySelector(".slide > .image-container > img")
+
+    elementoTitulo.textContent = novedadActual.titulo;
+    elementoParrafo.textContent = novedadActual.descripcion;
+    elementoImagen.src = novedadActual.url;
+    elementoImagen.alt = novedadActual.titulo;
+
+    setTimeout(loopSlides, 8000, novedades);
 }
 
-setInterval(slider(), 8000);
+async function importarJson(url) {
+    return fetch(url)
+        .then(response => response.json());
+}
+
+(async() => {
+    const novedades = await importarJson('./assets/js/novedades.json');
+    loopSlides(novedades);
+})();
