14d1120aed12a17c3c72f7a077e0206dfff2ce6d
Axel-Jeremias <axeljeremias1@gmail.com>
2023-10-13 21:22:06 -0300

    Usar closure para slider

--- assets/js/slider.js
+++ assets/js/slider.js
@@ -1,24 +1,24 @@
-let currentSlider = 1;
-
-function loopSlides(novedades) {
-    if (currentSlider >= novedades.length) {
-        currentSlider = 1;
-    } else {
-        currentSlider++;
-    }
-
-    const novedadActual = novedades[currentSlider - 1];
+function iniciarLoop(novedades) {
+    let currentSlider = 1;
 
     const elementoTitulo = document.querySelector(".slide > .info-slide > h3");
     const elementoParrafo = document.querySelector(".slide > .info-slide > p");
-    const elementoImagen = document.querySelector(".slide > .image-container > img")
+    const elementoImagen = document.querySelector(".slide > .image-container > img");
 
-    elementoTitulo.textContent = novedadActual.titulo;
-    elementoParrafo.textContent = novedadActual.descripcion;
-    elementoImagen.src = novedadActual.url;
-    elementoImagen.alt = novedadActual.titulo;
+    return function siguienteSlide() {
+        if (currentSlider >= novedades.length) {
+            currentSlider = 1;
+        } else {
+            currentSlider++;
+        }
 
-    setTimeout(loopSlides, 8000, novedades);
+        const novedadActual = novedades[currentSlider - 1];
+
+        elementoTitulo.textContent = novedadActual.titulo;
+        elementoParrafo.textContent = novedadActual.descripcion;
+        elementoImagen.src = novedadActual.url;
+        elementoImagen.alt = novedadActual.titulo;
+    }
 }
 
 async function importarJson(url) {
@@ -28,5 +28,7 @@ async function importarJson(url) {
 
 (async() => {
     const novedades = await importarJson('./assets/js/novedades.json');
-    loopSlides(novedades);
+    const loop = iniciarLoop(novedades);
+    loop();
+    setInterval(loop, 8000);
 })();
